e473aee933e819a3fa27434450acb64e29a3144b
tenmakenzou <generous35@yahoo.com>
2025-01-24 18:18:49 +0200

    changes

---
+++ frontend/src/Compoments/cartTab.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export const CartTab = () => {
+  return <div>CartTab</div>;
+};
+
+
---
+++ frontend/src/Compoments/header.js
@@ -0,0 +1,19 @@
+// header.js
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Cart from './pages/images/Cart.png';
+
+const Header = () => {
+  return( 
+  
+  <header className='flex-justify-between items-center mb-5 '>
+    <Link to="/" className='text-xl font-semibold'>Home</Link>
+    <div className='w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center position-relative'>
+        <img src = {Cart} alt = "" className='w-6'/>
+       
+        <span className='absolute  top-2/3 right-1/2 bg-blue-500  text-white text-sm  w-5 h-5 rounded-full' > 0 </span>
+    </div>
+    </header>)
+};
+
+export default Header;
